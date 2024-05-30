31aaf94fe94f45669228038b400ec77c463f21e8
Lucifer <listopadov2744@mail.com>
2024-05-29 23:15:23 -0800

    Добавление всех файлов в репозиторий

---
+++ test.js
@@ -0,0 +1,35 @@
+const http = require('http');
+
+let requestCount = 0;
+
+const server = http.createServer((request, response) => {
+    if (request.url === '/favicon.ico') {
+        // Відповідаємо на запит favicon.ico, щоб уникнути збільшення лічильника запитів
+        response.writeHead(204); // 204 No Content
+        response.end();
+        return;
+    }
+
+    requestCount++;
+
+    switch (request.url) {
+        case '/students':
+            response.write('STUDENT')
+            break
+        case '/':
+        case '/courses':
+            response.write('FRONT + BACK')
+            break
+        default:
+            response.write('404 not found')
+
+
+
+    }
+
+    response.end();
+});
+
+server.listen(3003, () => {
+    console.log('Server is listening on port 3003');
+});
